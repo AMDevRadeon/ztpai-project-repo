cd30892bf9c467d783f13456c490e077855e0591
RK <radoslaw.karpinski@student.pk.edu.pl>
2025-05-08 08:33:04 +0200

    fix(errors): add global exception handler

---
+++ app/src/EventListener/ExceptionListener.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+class ExceptionListener
+{
+    public function __invoke(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $jsonMessage = json_encode(
+            [
+                "desc" => $exception->getMessage(),
+                "code" => $exception->getCode()
+            ]
+        );
+
+        $response = new Response();
+        $response->setContent($jsonMessage);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $response->headers->set('Content-Type', 'application/json; charset=utf-8');
+
+        $event->setResponse($response);
+    }
+}
