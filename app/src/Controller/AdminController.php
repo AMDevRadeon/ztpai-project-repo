24c55a3088e4cb600f318607fed530bd63235305
RK <radoslaw.karpinski@student.pk.edu.pl>
2025-05-21 23:49:48 +0200

    feat: add endpoint for deleting users

---
+++ app/src/Controller/AdminController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Entity\UserRole;
+use App\Entity\UserSettings;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+final class AdminController extends AbstractController
+{
+    #[Route('api/admin/delete', name: 'api_admin_delete', methods: ['DELETE'])]
+    public function deleteUser(Request $req,
+                               TokenInterface $sec,
+                               UserRepository $repo,
+                               EntityManagerInterface $em): JsonResponse
+    {
+        $user = $sec->getUser();
+        if (!$user) 
+        { 
+            return $this->json(['desc' => 'Unauthorized', 'code' => Response::HTTP_UNAUTHORIZED], 
+                               Response::HTTP_UNAUTHORIZED);
+        }
+
+        $payload = $req->toArray();
+
+        // Required fields
+        if (empty($payload["uid"])) 
+        {
+            return $this->json(["desc" => "Missing $field", 'code' => Response::HTTP_UNAUTHORIZED],
+                               Response::HTTP_BAD_REQUEST);
+        }
+
+        $u = $repo->find($payload["uid"]);
+        if (!$u) 
+        {
+            return $this->json(['desc' => 'User not found', 'code' => Response::HTTP_BAD_REQUEST],
+                               Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($u->getPasshash() === '-')
+        {
+            return $this->json(['desc' => 'Already deleted', 'code' => Response::HTTP_BAD_REQUEST],
+                               Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($user->getUid() === $u->getUid())
+        {
+            return $this->json(['desc' => 'Trying to delete self', 'code' => Response::HTTP_BAD_REQUEST],
+                               Response::HTTP_BAD_REQUEST);
+        }
+
+        $u->setNick('Deleted_User_' . strval(time()));
+        $u->setPasshash('-');
+        $u->setProvenance('');
+        $u->setMotto('');
+        $u->getSettings()->setDisplayEmail(false);
+
+        $em->persist($u);
+        $em->flush();
+
+        return $this->json(['desc' => 'Deleted', 'code' => Response::HTTP_OK]);
+    }
+}
