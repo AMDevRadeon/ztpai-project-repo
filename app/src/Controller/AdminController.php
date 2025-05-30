431c42018a17557c8a6ddbafc3a65cc87f9336a2
RK <radoslaw.karpinski@student.pk.edu.pl>
2025-05-30 18:31:51 +0200

    feat: add endpoints for managing topics

--- app/src/Controller/AdminController.php
+++ app/src/Controller/AdminController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Entity\UserRole;
 use App\Entity\UserSettings;
+use App\Entity\Topic;
 use App\Repository\UserRepository;
+use App\Repository\TopicRepository;
+use App\Service\ValidJSONStructure;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -69,7 +73,7 @@ final class AdminController extends AbstractController
         )
     )]
     #[OA\Tag(name: 'Admin')]
-    #[Route('api/admin/delete', name: 'api_admin_delete', methods: ['DELETE'])]
+    #[Route('api/admin/user/delete', name: 'api_admin_user_delete', methods: ['DELETE'])]
     public function deleteUser(Request $req,
                                TokenInterface $sec,
                                UserRepository $repo,
@@ -84,10 +88,12 @@ final class AdminController extends AbstractController
 
         $payload = $req->toArray();
 
+        $missing_key = ValidJSONStructure::checkKeys($payload, 'uid');
+
         // Required fields
-        if (empty($payload["uid"])) 
+        if ($missing_key !== NULL) 
         {
-            return $this->json(["desc" => "Missing $field", 'code' => Response::HTTP_BAD_REQUEST],
+            return $this->json(["desc" => "Missing $missing_key", 'code' => Response::HTTP_BAD_REQUEST],
                                Response::HTTP_BAD_REQUEST);
         }
 
@@ -120,5 +126,137 @@ final class AdminController extends AbstractController
         $em->flush();
 
         return $this->json(['desc' => 'Deleted', 'code' => Response::HTTP_OK]);
+    }
+
+
+    #[OA\Tag(name: 'Admin')]
+    #[Route('api/admin/topic/add', name: 'api_admin_topic_add', methods: ['POST'])]
+    public function addTopic(Request $req,
+                             TokenInterface $sec,
+                             ValidatorInterface $validator,
+                             EntityManagerInterface $em): JsonResponse
+    {
+        $user = $sec->getUser();
+        if (!$user) 
+        { 
+            return $this->json(['desc' => 'Unauthorized', 'code' => Response::HTTP_UNAUTHORIZED], 
+                               Response::HTTP_UNAUTHORIZED);
+        }
+
+        $payload = $req->toArray();
+
+        $missing_key = ValidJSONStructure::checkKeys($payload, 'title', 'content');
+
+        if ($missing_key !== NULL)
+        {
+            return $this->json(["desc" => "Missing $missing_key", 'code' => Response::HTTP_BAD_REQUEST],
+                               Response::HTTP_BAD_REQUEST);
+        }
+
+        $topic = (new Topic())
+            ->setUid($user->getUid())
+            ->setTitle(strval($payload['title']))
+            ->setContent(strval($payload['content']))
+            ->setUser($user);
+
+        $errors = $validator->validate($topic);
+        if (count($errors) > 0) {
+            return $this->json(['desc' => $errors->get(0)->getPropertyPath() . ': ' . $errors->get(0)->getMessage(), 'code' => Response::HTTP_UNPROCESSABLE_ENTITY],
+                               Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $em->persist($topic);
+        $em->flush();
+
+        return $this->json(['desc' => "Created new topic: " . $topic->getTitle(), 'code' => Response::HTTP_CREATED],
+                           Response::HTTP_CREATED);
+    }
+
+
+    #[OA\Tag(name: 'Admin')]
+    #[Route('api/admin/topic/delete', name: 'api_admin_topic_delete', methods: ['DELETE'])]
+    public function deleteTopic(Request $req,
+                                TokenInterface $sec,
+                                TopicRepository $repo,
+                                ValidatorInterface $validator,
+                                EntityManagerInterface $em): JsonResponse
+    {
+        $user = $sec->getUser();
+        if (!$user) 
+        { 
+            return $this->json(['desc' => 'Unauthorized', 'code' => Response::HTTP_UNAUTHORIZED], 
+                               Response::HTTP_UNAUTHORIZED);
+        }
+
+        $payload = $req->toArray();
+
+        $missing_key = ValidJSONStructure::checkKeys($payload, 'tid');
+
+        if ($missing_key !== NULL)
+        {
+            return $this->json(["desc" => "Missing $missing_key", 'code' => Response::HTTP_BAD_REQUEST],
+                               Response::HTTP_BAD_REQUEST);
+        }
+
+        $topic = $repo->find($payload['tid']);
+
+        if (!$topic) {
+            return $this->json(['desc' => 'Topic not found', 'code' => Response::HTTP_BAD_REQUEST],
+                                Response::HTTP_BAD_REQUEST);
+        }
+
+        $em->remove($topic);
+        $em->flush();
+
+        return $this->json(['desc' => 'Deleted', 'code' => Response::HTTP_OK]);
+    }
+
+
+    #[OA\Tag(name: 'Admin')]
+    #[Route('api/admin/topic/edit', name: 'api_admin_topic_edit', methods: ['PATCH'])]
+    public function editTopic(Request $req,
+                              TokenInterface $sec,
+                              TopicRepository $repo,
+                              ValidatorInterface $validator,
+                              EntityManagerInterface $em): JsonResponse
+    {
+        $user = $sec->getUser();
+        if (!$user) 
+        { 
+            return $this->json(['desc' => 'Unauthorized', 'code' => Response::HTTP_UNAUTHORIZED], 
+                               Response::HTTP_UNAUTHORIZED);
+        }
+
+        $payload = $req->toArray();
+
+        $missing_key = ValidJSONStructure::checkKeys($payload, 'tid');
+
+        if ($missing_key !== NULL)
+        {
+            return $this->json(["desc" => "Missing $missing_key", 'code' => Response::HTTP_BAD_REQUEST],
+                               Response::HTTP_BAD_REQUEST);
+        }
+
+        $topic = $repo->find($payload['tid']);
+
+        if (!$topic) {
+            return $this->json(['desc' => 'Topic not found', 'code' => Response::HTTP_BAD_REQUEST],
+                                Response::HTTP_BAD_REQUEST);
+        }
+
+        if (isset($payload['title'])) 
+        {
+            $topic->setTitle($payload['title'] ?: null);
+        }
+
+        if (isset($payload['content'])) 
+        {
+            $topic->setContent($payload['content'] ?: null);
+        }
+
+        $em->persist($topic);
+        $em->flush();
+
+        return $this->json(['desc' => 'Updated', 'code' => Response::HTTP_OK]);
     }
 }
