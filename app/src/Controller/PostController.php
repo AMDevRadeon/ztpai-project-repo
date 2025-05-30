2acbab275ce2f76d570797e07dc722ef46da86a9
RK <radoslaw.karpinski@student.pk.edu.pl>
2025-05-30 20:35:38 +0200

    feat: add endpoint for listing posts in given topic

---
+++ app/src/Controller/PostController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Topic;
+use App\Entity\Post;
+use App\Service\ValidJSONStructure;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+use App\Repository\TopicRepository;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Nelmio\ApiDocBundle\Attribute\Model;
+use Nelmio\ApiDocBundle\Attribute\Security;
+use OpenApi\Attributes as OA;
+
+
+final class PostController extends AbstractController
+{
+    #[Route('api/post/get', name: 'api_get_post', methods: ['POST'])]
+    public function getTopics(Request $req,
+                              EntityManagerInterface $em,
+                              TopicRepository $repo): JsonResponse
+    {
+        $payload = $req->toArray();
+
+        $missing_key = ValidJSONStructure::checkKeys($payload, 'tid', 'offset', 'limit');
+
+        if ($missing_key !== NULL)
+        {
+            return $this->json(["desc" => "Missing $missing_key", 'code' => Response::HTTP_BAD_REQUEST],
+                               Response::HTTP_BAD_REQUEST);
+        }
+
+        $offset = intval($payload["offset"]);
+        $limit = intval($payload["limit"]);
+
+        $limit = $limit > 128 ? 128 : $limit;
+
+        $topic = $repo->find($payload['tid']);
+
+        if (!$topic) 
+        {
+            return $this->json(['desc' => 'Topic not found', 'code' => Response::HTTP_BAD_REQUEST],
+                                Response::HTTP_BAD_REQUEST);
+        }
+
+        // if ($topic->getIsArchived())
+        // {
+        //     return $this->json(['desc' => 'Topic is archived (read only)', 'code' => Response::HTTP_BAD_REQUEST],
+        //                         Response::HTTP_BAD_REQUEST);
+        // }
+
+        // TODO: put in database directory or sumfin, i dunno
+        $query_builder = $em->createQueryBuilder();
+        $result_query = $query_builder
+            ->select('t.pid', 't.uid', 't.postCreationTimestamp', 't.title', 't.content', 't.isArchived', 't.isClosed')
+            ->from(Post::class, 't')
+            ->where('t.tid = :current_tid')
+            ->orderBy('t.postCreationTimestamp', 'DESC')
+            ->setParameter('current_tid', $topic->getTid())
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult();
+
+        $data = [
+            'tid' => $topic->getTid(),
+            'count' => count($result_query),
+            'posts' => $result_query
+        ];
+
+        return $this->json($data);
+    }
+}
